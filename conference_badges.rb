990849cc5b743fa4d89610f8ff7c2f4a5d20ac25
George Fernandez <fernandezg704@gmail.com>
2022-10-12 15:45:25 -0400

    commit tree d3d77387efe24c3c452281c8f75b11f47402f305

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker name
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    name_list = []
+    names.map do |name|
+        name_list << "Hello, my name is #{name}."
+    end
+    return name_list
+end
